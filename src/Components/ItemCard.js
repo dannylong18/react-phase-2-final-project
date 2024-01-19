b232b3a533159cdd2fd05c7de7e280b319e5deab
Daniel Long <danielwlong618@gmail.com>
2024-01-18 22:03:09 -0600

    itemcard component updated with basic name, image, description, price and link elements

---
+++ src/Components/ItemCard.js
@@ -0,0 +1,12 @@
+
+function ItemCard () {
+    return (
+        <div>
+            <h3>Item Card Name</h3>
+            <img src="https://via.placeholder.com/150x150" alt="Item Image" width='200px' height='200px'/>
+            <p>Item Description</p>
+            <p>Item Price</p>
+            <a>Item Link</a>
+        </div>
+    )
+}
