04c9f5cab6d7e0e38dfd2de9ce972a44dc2e898a
Daniel Long <danielwlong618@gmail.com>
2024-01-20 15:37:28 -0600

    fetch request created

--- src/Components/FavoriteItems.js
+++ src/Components/FavoriteItems.js
@@ -1,9 +1,24 @@
 import NavBar from './NavBar';
+import { useState, useEffect } from'react';
 
 function FavoriteItems () {
+    
+    useEffect(() => {
+        fetch('http://localhost:3000/items')
+        .then (response => response.json())
+        .then (data => {
+             console.log(data)
+        })
+    }, [])
+    
     return (
         <div>
+            <header>
             <NavBar />
+            </header>
+            <main>
+
+            </main>
         </div>
     )
 }
