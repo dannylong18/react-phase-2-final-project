614f299ff96922b6851b520fdb2dee3fc188a8bc
Daniel Long <danielwlong618@gmail.com>
2024-01-20 22:42:41 -0600

    favorite items component updated with fetch request and state; props passed to item card to create display

--- src/Components/FavoriteItems.js
+++ src/Components/FavoriteItems.js
@@ -1,23 +1,39 @@
 import NavBar from './NavBar';
 import { useState, useEffect } from'react';
+import ItemCard from './ItemCard';
 
 function FavoriteItems () {
-    
+    const [items, setItems] = useState([])
+
     useEffect(() => {
         fetch('http://localhost:3000/items')
         .then (response => response.json())
         .then (data => {
-             console.log(data)
+             setItems(data)
         })
     }, [])
-    
+
+    const displayItems = () => {
+        return items.map(item => {
+            return (
+                <ItemCard 
+                key={item.id} 
+                description={item.description} 
+                image={item.image} 
+                link={item.link} 
+                name={item.name} 
+                price={item.price} 
+                />
+            )
+        })
+    }
     return (
         <div>
             <header>
             <NavBar />
             </header>
             <main>
-
+            {displayItems()}
             </main>
         </div>
     )
--- src/Components/ItemCard.js
+++ src/Components/ItemCard.js
@@ -1,12 +1,12 @@
 
-function ItemCard () {
+function ItemCard ( {description, image, link, name, price}) {
     return (
         <div>
-            <h3>Item Card Name</h3>
-            <img src="https://via.placeholder.com/150x150" alt="Item Image" width='200px' height='200px'/>
-            <p>Item Description</p>
-            <p>Item Price</p>
-            <a>Item Link</a>
+            <h3>{name}</h3>
+            <img src={image} alt="Item" width='200px' height='200px'/>
+            <p>{description}</p>
+            <p>Price: {price}</p>
+            <a href={link}>Item Link</a>
         </div>
     )
 }
